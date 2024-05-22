a3db3e1e42d6181ff262124e70b662b9db1c318a
Rashedul Islam <rashedul.islam@bracits.com>
2024-05-23 01:39:41 +0600

    Added generic method in basePage class

--- src/test/java/com/parabankl/pages/BasePage.java
+++ src/test/java/com/parabankl/pages/BasePage.java
@@ -1,4 +1,32 @@
 package com.parabankl.pages;
 
-public class BasePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public abstract class BasePage {
+    public WebDriver driver;
+
+    //Constructor
+    BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //get every Page title
+    public abstract String getPageTitle();
+
+    //Input value in Input field
+    public void inputText(By element, String text) {
+        driver.findElement(element).sendKeys(text);
+    }
+
+    //Click on any link or button
+    public void clickBtn(By element) {
+        driver.findElement(element).click();
+    }
+
+    //Get text
+    public String getText(By element){
+        return driver.findElement(element).getText();
+    }
+
 }
