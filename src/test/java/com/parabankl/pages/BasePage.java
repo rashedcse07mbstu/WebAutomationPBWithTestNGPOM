4b7a1643ddb8cc1a89d0eddf7ae2b1c6c74a680a
Rashedul Islam <rashedul.islam@bracits.com>
2024-05-23 22:59:19 +0600

    Added generic method

--- src/test/java/com/parabankl/pages/BasePage.java
+++ src/test/java/com/parabankl/pages/BasePage.java
@@ -1,7 +1,13 @@
 package com.parabankl.pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public abstract class BasePage {
     public WebDriver driver;
@@ -25,8 +31,39 @@ public abstract class BasePage {
     }
 
     //Get text
-    public String getText(By element){
+    public String getText(By element) {
         return driver.findElement(element).getText();
     }
 
+    //select dropdown value from list
+    public void selectValueByIndex(WebElement element, int index) {
+        Select selectEle = new Select(element);
+        selectEle.selectByIndex(1);
+    }
+
+    public void selectValueByValue(WebElement element, String value) {
+        Select selectEle = new Select(element);
+        selectEle.selectByValue(value);
+    }
+
+    public void selectValueByVisibleText(WebElement element, String text) {
+        Select selectEle = new Select(element);
+        selectEle.selectByValue(text);
+    }
+
+    //Get error message
+    public String getErrorMessage(By element) {
+        return driver.findElement(element).getText();
+    }
+
+    //Take screenshot
+    public static String getScreenShot(WebDriver driver, String screenShotName) throws IOException {
+        String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        String source = ts.getScreenshotAs(OutputType.BASE64);
+        String destination = System.getProperty("user.dir") + "/Screenshots/" + screenShotName + dateName + ".png";
+        File fileDestination = new File(destination);
+        FileUtils.copyFile(new File(source), fileDestination);
+        return destination;
+    }
 }
