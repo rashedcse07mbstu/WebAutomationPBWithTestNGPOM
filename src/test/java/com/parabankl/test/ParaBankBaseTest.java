0c08f6be209e8eee0939e526d625c7781c15f775
Rashedul Islam <rashedul.islam@bracits.com>
2024-06-02 19:07:59 +0600

    Added browser setup info

--- src/test/java/com/parabankl/test/ParaBankBaseTest.java
+++ src/test/java/com/parabankl/test/ParaBankBaseTest.java
@@ -2,6 +2,7 @@ package com.parabankl.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -34,6 +35,10 @@ public class ParaBankBaseTest {
         browserName = properties.getProperty("browserName");
         if (browserName.equals("firefox")) {
             WebDriverManager.firefoxdriver().clearDriverCache().browserVersion("107").setup();
+        } else if (browserName.equals("chrome")) {
+            //initializing and starting the chrome browser
+            WebDriverManager.chromedriver().clearDriverCache().browserVersion("107").setup();
+            driver = new ChromeDriver();
         }
 
     }
