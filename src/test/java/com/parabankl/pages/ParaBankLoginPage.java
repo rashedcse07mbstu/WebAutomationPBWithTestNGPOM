740acc44d93afef4f40f5234d1e0f44242a3a3ae
Rashedul Islam <rashedul.islam@bracits.com>
2024-06-26 01:27:04 +0600

    Added login Page, Login Test class

--- src/test/java/com/parabankl/pages/ParaBankLoginPage.java
+++ src/test/java/com/parabankl/pages/ParaBankLoginPage.java
@@ -1,4 +1,30 @@
 package com.parabankl.pages;
 
-public class ParaBankLoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ParaBankLoginPage extends BasePage {
+    public ParaBankLoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    By userName = By.id("username");
+    By password = By.id("inputPassword");
+    By loginBtn = By.id("submit-login-form");
+
+    public ParaBankLoginPage fillUserName(String user) {
+        inputText(userName, user);
+        return this;
+    }
+
+    public ParaBankLoginPage fillPassword(String pas) {
+        inputText(password, pas);
+        return this;
+    }
+
+    public AccountOverviewPage clickLoginBtn() {
+        clickBtn(loginBtn);
+        return getInstance(AccountOverviewPage.class);
+    }
+
 }
