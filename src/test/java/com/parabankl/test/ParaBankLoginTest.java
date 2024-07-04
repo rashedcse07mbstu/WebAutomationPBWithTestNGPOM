83f8b09e852dc0beea06a99d84a52b96459e1f18
Rashedul Islam <rashedul.islam@bracits.com>
2024-07-05 01:11:03 +0600

    Added Login Test Case

--- src/test/java/com/parabankl/test/ParaBankLoginTest.java
+++ src/test/java/com/parabankl/test/ParaBankLoginTest.java
@@ -2,8 +2,19 @@ package com.parabankl.test;
 
 import com.parabankl.pages.AccountOverviewPage;
 import com.parabankl.pages.ParaBankLoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class ParaBankLoginTest {
+public class ParaBankLoginTest extends ParaBankBaseTest {
     ParaBankLoginPage paraBankLoginPage;
     AccountOverviewPage accountOverviewPage;
+
+    @Test
+    public void loginShouldSucceed() {
+        paraBankLoginPage = page.getInstance(ParaBankLoginPage.class);
+        paraBankLoginPage.fillUserName(System.getProperty("userName"));
+        paraBankLoginPage.fillPassword(System.getProperty("password"));
+        paraBankLoginPage.clickLoginBtn();
+        Assert.assertTrue(paraBankLoginPage.getPageTitle().contains(driver.getTitle()));
+    }
 }
